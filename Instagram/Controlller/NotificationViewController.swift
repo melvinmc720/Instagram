b8fd36a3ff9d9ad9b14eb33cdd581b859dbb4361
melvinmc <melvinmc720@gmail.com>
2025-02-08 00:15:31 -0500

    Notification

--- Instagram/Controlller/NotificationViewController.swift
+++ Instagram/Controlller/NotificationViewController.swift
@@ -7,23 +7,45 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
+class NotificationViewController: UITableViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configuration()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Helpers
+    
+    func configuration(){
+        
+        view.backgroundColor = .white
+        navigationItem.title = "Notification"
+        
+        
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: NotificationCell.identifier)
+        tableView.rowHeight = 80
+        tableView.separatorStyle = .none
+        
+        
     }
-    */
+    
+}
 
+extension NotificationViewController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: NotificationCell.identifier,
+            for: indexPath
+        ) as? NotificationCell else { return UITableViewCell()}
+        
+
+        return cell
+    }
 }
